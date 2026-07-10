39d5480f22ae83d455679f60ce4f8cb16745170f
Jose Ramirez Linares <ramirezjose.web@gmail.com>
2026-07-10 16:17:25 +0200

    First model created business.py

---
+++ backend/app/models/business.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Integer, String, DateTime
+
+from app.db.database import Base # importa la clase Base desde el archivo database.py, lo que permite que la clase Business herede de ella y se convierta en una tabla de la base de datos.
+
+class Business(Base): 
+    __tablename__ = "businesses"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True) # Mapped es un tipo de columna que se utiliza para mapear una columna de la base de datos a un atributo de la clase. En este caso, se está mapeando la columna id de la tabla businesses a un atributo id de la clase Business. El parámetro primary_key=True indica que esta columna es la clave primaria de la tabla, y el parámetro index=True indica que se debe crear un índice en esta columna para mejorar el rendimiento de las consultas.
+    name: Mapped[str] = mapped_column(String(150), nullable=False)
+    category: Mapped[str] = mapped_column(String(50), nullable=False)
+    location: Mapped[str | None] = mapped_column(String(150), nullable=True)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime, 
+        default=datetime.utcnow,
+        nullable=False, 
+        )
